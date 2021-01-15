d6a116ec5b019f04afbfe82e25ef4d0c487d6ed0
Gabriel Barker <gabrielbarker@gmail.com>
2021-01-15 01:15:23 +0000

    ADD hand class

---
+++ snap/hand.py
@@ -0,0 +1,28 @@
+from snap.card import Card
+
+
+class Hand:
+    def __init__(self, cards=[]):
+        self._cards = cards
+
+    def add(self, card):
+        return self._cards.append(card)
+
+    def remove(self, index):
+        return self._cards.pop(index)
+
+    def get(self, index):
+        return self._cards[index]
+
+    def count(self):
+        return len(self._cards)
+
+    def hide_all(self):
+        [card.hide() for card in self._cards]
+
+    def strings(self):
+        strings = ["", "", "", ""]
+        for card in self._cards:
+            for index, string in enumerate(card.strings()):
+                strings[index] += string
+        return strings
