cd02df4fb09f32d6d2a16d2bae64aff11a5c2f66
Gabriel Barker <gabrielbarker@gmail.com>
2021-01-15 01:15:31 +0000

    ADD grid class

---
+++ snap/grid.py
@@ -0,0 +1,30 @@
+from random import shuffle, sample
+from snap.card import Card
+from snap.hand import Hand
+
+
+class Grid:
+    def __init__(self, width):
+        self._width = width
+        self._hands = self._get_new_hands()
+
+    def get(self, p):
+        return self._hands[p.y()].get(p.x())
+
+    def hide_all(self):
+        [hand.hide_all() for hand in self._hands]
+
+    def strings(self):
+        strings = []
+        for hand in self._hands:
+            strings += hand.strings()
+        return strings
+
+    def _get_new_hands(self):
+        indices = sample(range(52), self._width ** 2) * 2
+        shuffle(indices)
+        hands = []
+        for i in range(self._width):
+            row = indices[i * self._width * 2 : (i + 1) * self._width * 2]
+            hands.append(Hand([Card(j) for j in row]))
+        return hands
